3e7ee00d871491b8b4f19d353bd361f976d965e1
VKhandzhian <hvladimir070@gmail.com>
2023-09-02 18:43:13 +0300

    Commit #2: Move chat GPT API key to .env.  Delete console.log from app. Rewrite " as ' in the chatGPT.service.js.

--- src/services/chatGPT.service.js
+++ src/services/chatGPT.service.js
@@ -1,23 +1,21 @@
-const API_KEY = '{{KEY}}'
-
 const sendQuestionToChatGPT = async (question) => {
 
     const payload = {
-        "model": "gpt-3.5-turbo",
-        "temperature" : 1.0,
-        "messages" : [
+        'model': 'gpt-3.5-turbo',
+        'temperature' : 1.0,
+        'messages' : [
             { 
-                "role": "system",
-                "content": question 
+                'role': 'system',
+                'content': question 
             }
         ]  
     }
 
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
-        method: "POST",
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+        method: 'POST',
         headers: {
-          "Authorization": "Bearer " + API_KEY,
-          "Content-Type": "application/json",
+          'Authorization': 'Bearer ' + process.env.REACT_APP_CHAT_GPT_API_KEY,
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(payload),
     });
