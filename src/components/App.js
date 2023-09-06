e895982cc68556a42fc6f2862e0c8579eca625a6
VKhandzhian <hvladimir070@gmail.com>
2023-09-06 20:53:37 +0300

    Commit#6: Add logic for changing number of columns and rows

--- src/components/App.js
+++ src/components/App.js
@@ -9,18 +9,34 @@ import sendQuestionToChatGPT from '../services/chatGPT.service';
 import Textarea from './Textarea/Textarea';
 import Label from './Label/Label';
 import Button from './Button/Button';
+import textareaSizes from '../constants/textarea.constant';
 
 const App = () => {
 
     const [question, setQuestion] = useState('');
     const [answer, setAnswer] = useState('');
+    const [answerAreaSize, setAnswerAreaSize] = useState({rows: textareaSizes.MIN_ROW_NUMBER, columns: textareaSizes.MIN_COLUMN_NUMBER});
 
     const enterQuestion = (event) => {
         setQuestion(event.target.value);
     }
 
     const questionHandler = async () => {
-        setAnswer(await sendQuestionToChatGPT(question));    
+        const responseFromChatGPT = await sendQuestionToChatGPT(question);
+        const currentAnswerAreaSize = answerAreaSize.rows * answerAreaSize.columns;    
+    
+        if (currentAnswerAreaSize < responseFromChatGPT.length) {
+            const newColumnNumber = responseFromChatGPT.length / answerAreaSize.rows;
+
+            if (newColumnNumber > textareaSizes.MAX_COLUMN_NUMBER) {
+                const newRowNumber = responseFromChatGPT.length / textareaSizes.MAX_COLUMN_NUMBER;
+                setAnswerAreaSize({rows: newRowNumber, columns: textareaSizes.MAX_COLUMN_NUMBER});
+            } else {
+                setAnswerAreaSize(prevSize => { return {...prevSize, columns: newColumnNumber}; });
+            }
+        }
+
+        setAnswer(responseFromChatGPT);    
     }
 
     return (
@@ -50,11 +66,12 @@ const App = () => {
               <div>
                   <Textarea 
                       className='question-answer-textarea' 
-                      isDisabled={true} isReadOnly={true} 
+                      isDisabled={true}
+                      isReadOnly={true} 
                       id='answer' 
                       name='answer' 
-                      rows='5' 
-                      cols='33' 
+                      rows={answerAreaSize.rows} 
+                      cols={answerAreaSize.columns}
                       value={answer} 
                       />
               </div>
